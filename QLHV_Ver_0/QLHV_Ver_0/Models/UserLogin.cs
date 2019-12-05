2b79de9c4ffb7a5e8098cce85e3613b1877f79d6
KhoaNguyen <48175093+KhoaNguyen1999@users.noreply.github.com>
2019-12-05 14:18:52 +0700

    Test chức năng login
    Test layout mvc 5

---
+++ QLHV_Ver_0/QLHV_Ver_0/Models/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLHV_Ver_0.Models
+{
+    public class UserLogin
+    {
+        public string Name { get; set; }
+        public int Password { get; set; }
+    }
+}
