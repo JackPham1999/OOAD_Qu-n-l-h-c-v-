df2e3ccd15bef03ffc202ca0f030bf7085aa5ada
KhoaNguyen <48175093+KhoaNguyen1999@users.noreply.github.com>
2019-12-06 12:09:54 +0700

    Login ASP.NET Core
    Thay đổi ASP.NET thành ASP.NET Crre

---
+++ QLHV_OOAD_Core/QLHV_OOAD_Core/Controllers/ValidationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QLHV_OOAD_Core.Controllers
+{
+    public class ValidationController : Controller
+    {
+        public IActionResult ValidateForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return Content("A");
+        }
+    }
+}
