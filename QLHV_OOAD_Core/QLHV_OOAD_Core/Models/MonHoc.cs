66cfb5838af929104e211f194dae9f85852da659
huybuid <48416664+huybuid@users.noreply.github.com>
2019-12-11 14:50:33 +0700

    Sửa _Layout.cshtml, TestController
    Sửa Layout để sử dụng cho View
    Làm thử 1 cái TestController

---
+++ QLHV_OOAD_Core/QLHV_OOAD_Core/Models/MonHoc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLHV_OOAD_Core.Models
+{
+    public class MonHoc
+    {
+        public int IDMH { get; set; }
+        public string TenMonHoc { get; set; }
+    }
+}
